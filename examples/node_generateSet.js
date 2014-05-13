458207a49a681b782f058739b7a2270e5cee0016
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-05-13 17:44:06 +0200

    Bee Search Dataset

--- examples/node_generateSet.js
+++ examples/node_generateSet.js
@@ -3,18 +3,44 @@ var sys = require('sys')
 
 var Faker= require('../index');
 
+var length = process.argv[2];
+var logs = process.argv[3];
 
-// generate dataSet as example
-fs.writeFile('../examples/dataSet.json',  JSON.stringify(Faker.Helpers.userCard()), function() {
-  sys.puts("dataSet generated successfully!");
-});
+fs.unlink('bigDataSet.json');
+
 // generate bigDataSet as example
 var bigSet = [];
+for (var i = length - 1; i >= 0; i--) {
+  var client = {};
+  client.firstname = Faker.random.first_name();
+  client.lastname = Faker.random.last_name();
+  client.email = Faker.Helpers.slugify(client.firstname.toLowerCase()) + "." + Faker.Helpers.slugify(client.lastname.toLowerCase()) + "@" + Faker.Helpers.slugify(Faker.Internet.domainName());
+  client.company = {};
+  client.company.name = Faker.Company.companyName();
+  client.company.catchPhrase = Faker.Company.catchPhrase();
+  client.company.siren = Faker.Helpers.replaceSymbolWithNumber("### ### ###");
+  client.adress = {};
+  client.adress.street = Faker.Address.streetAddress();
+  client.adress.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
+  client.adress.city = Faker.Address.city();
+  client.adress.state = Faker.random.fr_state();
 
-for(i = 20; i >= 0; i--){
-  bigSet.push(Faker.Helpers.userCard());
+  if (logs) {
+    console.log(JSON.stringify(client));
+  };
+  
+  bigSet.push(client);
+
+  // Empty array in a file every 10 items
+  if (bigSet.length >= 0) {
+    fs.appendFile('bigDataSet.json', JSON.stringify(bigSet, null, 2), function(err) {
+      if (err) throw err;
+      // Log
+      console.log("appended bigDataSet.json");
+    });
+    // Empty the array ;)
+    bigSet.length = 0;
+  };
 };
 
-fs.writeFile('../examples/bigDataSet.json',  JSON.stringify(bigSet), function() {
-  sys.puts("bigDataSet generated successfully!");
-});
+console.log("bigDataSet.json generated successfully!");
