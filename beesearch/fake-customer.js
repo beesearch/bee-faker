bdcf653046dd56f5093a5f3988acc1961146d8b5
robbee <alexandre.bonte@beesearch.fr>
2014-06-21 15:48:58 +0200

    refactor customer model with order inside

--- beesearch/fake-customer.js
+++ beesearch/fake-customer.js
@@ -2,21 +2,21 @@ var fs = require('fs');
 var Faker= require('../index');
 var definitions = require('../lib/definitions');
 
-
 // Customer setup
 var subsidiary = process.argv[2];
 var customerLength = process.argv[3];
+var orderLength = process.argv[4];
 
 // check subsidiary arg
 if (subsidiary == "snrf") {
-  console.log("### Starting  generating " + customerLength + " customers for " + subsidiary);
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
   var fileName = 'fake-customer-' + subsidiary + '.json'
   //var product_qualif = "product_qualif_" + subsidiary;
 } else if (subsidiary == "fta") {
-  console.log("### Starting  generating " + customerLength + " customers for " + subsidiary);
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
   var fileName = 'fake-customer-' + subsidiary + '.json'
 } else if (subsidiary == "qn") {
-  console.log("### Starting  generating " + customerLength + " customers for " + subsidiary);
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
   var fileName = 'fake-customer-' + subsidiary + '.json'
 } else {
   console.log("### Error subsidiary must be 'snrf', 'fta' or 'qn'. Please retry...");
@@ -36,147 +36,196 @@ for (var i = 1; i <= customerLength; i++) {
   var customer = {};
   var customerName = Faker.Company.companyName();
   var customerInternetDomain = Faker.Helpers.slugify(customerName.toLowerCase() + "." + Faker.random.domain_suffix());
- 
+
   customer.company = {};
   customer.company.id = i;
   customer.company.name = customerName;
   customer.company.catchPhrase = Faker.Company.catchPhrase();
   customer.company.siren = Faker.Helpers.replaceSymbolWithNumber("### ### ###");
 
-   if (subsidiary == "snrf") {
-    customer.commercialAddress = {};
-    customer.commercialAddress.street = Faker.Address.streetAddress();
-    customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
-    customer.commercialAddress.city = Faker.Address.city();
-    customer.commercialAddress.state = Faker.random.fr_state();
-    customer.shippingAddress = {};
-    customer.shippingAddress.street = Faker.Address.streetAddress();
-    customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
-    customer.shippingAddress.city = Faker.Address.city();
-    customer.shippingAddress.state = Faker.random.fr_state();  
-  } else if (subsidiary == "fta") {
-    customer.commercialAddress = {};
-    customer.commercialAddress.street = Faker.Address.streetAddress();
-    customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
-    customer.commercialAddress.city = Faker.Address.city();
-    customer.commercialAddress.state = Faker.random.fr_state();
-    customer.shippingAddress = {};
-    customer.shippingAddress.street = Faker.Address.streetAddress();
-    customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
-    customer.shippingAddress.city = Faker.Address.city();
-    customer.shippingAddress.state = Faker.random.fr_state();   
+
+  customer.addresses = [];
+  if (subsidiary == "snrf" || subsidiary == "fta") {
+    address = {};
+    address.street = Faker.Address.streetAddress();
+    address.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
+    address.city = Faker.Address.city();
+    address.state = Faker.random.fr_state();
+    address.type = "sales";
+    customer.addresses.push(address);
+    address = {};
+    address.street = Faker.Address.streetAddress();
+    address.zip = "" + Faker.random.numberlowhigh(10, 98) + Faker.random.numberlowhigh(100, 999);
+    address.city = Faker.Address.city();
+    address.state = Faker.random.fr_state();
+    address.type = "shipping";
+    customer.addresses.push(address);
   } else if (subsidiary == "qn") {
-      var randomDept = Faker.random.number(5);
-      switch (randomDept) {
-        case 0:
-                customer.commercialAddress = {};
-                customer.commercialAddress.street = Faker.Address.streetAddress();
-                customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(44, 44) + Faker.random.numberlowhigh(100, 999);
-                customer.commercialAddress.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
-                customer.commercialAddress.state = "Pays-de-Loire";  
-                customer.shippingAddress = {};
-                customer.shippingAddress.street = Faker.Address.streetAddress();
-                customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(44, 44) + Faker.random.numberlowhigh(100, 999);
-                customer.shippingAddress.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
-                customer.shippingAddress.state = "Pays-de-Loire"; 
-            break;
-        case 1:
-                customer.commercialAddress = {};
-                customer.commercialAddress.street = Faker.Address.streetAddress();
-                customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(53, 53) + Faker.random.numberlowhigh(100, 999);
-                customer.commercialAddress.city = Faker.Helpers.shuffle(definitions.ville_mayenne).slice(0, 1)[0];
-                customer.commercialAddress.state = "Mayenne";  
-                customer.shippingAddress = {};
-                customer.shippingAddress.street = Faker.Address.streetAddress();
-                customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(53, 53) + Faker.random.numberlowhigh(100, 999);
-                customer.shippingAddress.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
-                customer.shippingAddress.state = "Mayenne";
-             break;
-        case 2:
-                customer.commercialAddress = {};
-                customer.commercialAddress.street = Faker.Address.streetAddress();
-                customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(85, 85) + Faker.random.numberlowhigh(100, 999);
-                customer.commercialAddress.city = Faker.Helpers.shuffle(definitions.ville_vendee).slice(0, 1)[0];
-                customer.commercialAddress.state = "Vendée";  
-                customer.shippingAddress = {};
-                customer.shippingAddress.street = Faker.Address.streetAddress();
-                customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(85, 85) + Faker.random.numberlowhigh(100, 999);
-                customer.shippingAddress.city = Faker.Helpers.shuffle(definitions.ville_vendee).slice(0, 1)[0];
-                customer.shippingAddress.state = "Vendée";
-             break;
-        case 3:
-                customer.commercialAddress = {};
-                customer.commercialAddress.street = Faker.Address.streetAddress();
-                customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(72, 72) + Faker.random.numberlowhigh(100, 999);
-                customer.commercialAddress.city = Faker.Helpers.shuffle(definitions.ville_sarthe).slice(0, 1)[0];
-                customer.commercialAddress.state = "Sarthe";  
-                customer.shippingAddress = {};
-                customer.shippingAddress.street = Faker.Address.streetAddress();
-                customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(72, 72) + Faker.random.numberlowhigh(100, 999);
-                customer.shippingAddress.city = Faker.Helpers.shuffle(definitions.ville_sarthe).slice(0, 1)[0];
-                customer.shippingAddress.state = "Sarthe";
-             break;
-        case 4:
-                customer.commercialAddress = {};
-                customer.commercialAddress.street = Faker.Address.streetAddress();
-                customer.commercialAddress.zip = "" + Faker.random.numberlowhigh(49, 49) + Faker.random.numberlowhigh(100, 999);
-                customer.commercialAddress.city = Faker.Helpers.shuffle(definitions.ville_maine_et_loire).slice(0, 1)[0];
-                customer.commercialAddress.state = "Maine et Loire";  
-                customer.shippingAddress = {};
-                customer.shippingAddress.street = Faker.Address.streetAddress();
-                customer.shippingAddress.zip = "" + Faker.random.numberlowhigh(49, 49) + Faker.random.numberlowhigh(100, 999);
-                customer.shippingAddress.city = Faker.Helpers.shuffle(definitions.ville_maine_et_loire).slice(0, 1)[0];
-                customer.shippingAddress.state = "Maine et Loire";
-             break;
-      }
+    var randomDept = Faker.random.number(5);
+    switch (randomDept) {
+      case 0:
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(44, 44) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
+      address.state = "Pays-de-Loire"; 
+      address.type = "sales";
+      customer.addresses.push(address);
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(44, 44) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
+      address.state = "Pays-de-Loire"; 
+      address.type = "shipping";
+      customer.addresses.push(address);
+      break;
+      case 1:
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(53, 53) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_mayenne).slice(0, 1)[0];
+      address.state = "Mayenne";  
+      address.type = "sales";
+      customer.addresses.push(address);
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(53, 53) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_loire_atlantique).slice(0, 1)[0];
+      address.state = "Mayenne";
+      address.type = "shipping";
+      customer.addresses.push(address);
+      break;
+      case 2:
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(85, 85) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_vendee).slice(0, 1)[0];
+      address.state = "Vendée"; 
+      address.type = "sales";
+      customer.addresses.push(address); 
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(85, 85) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_vendee).slice(0, 1)[0];
+      address.state = "Vendée";
+      address.type = "shipping";
+      customer.addresses.push(address);
+      break;
+      case 3:
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(72, 72) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_sarthe).slice(0, 1)[0];
+      address.state = "Sarthe";  
+      address.type = "sales";
+      customer.addresses.push(address);
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(72, 72) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_sarthe).slice(0, 1)[0];
+      address.state = "Sarthe";
+      address.type = "shipping";
+      customer.addresses.push(address);
+      break;
+      case 4:
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(49, 49) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_maine_et_loire).slice(0, 1)[0];
+      address.state = "Maine et Loire";  
+      address.type = "sales";
+      customer.addresses.push(address);
+      address = {};
+      address.street = Faker.Address.streetAddress();
+      address.zip = "" + Faker.random.numberlowhigh(49, 49) + Faker.random.numberlowhigh(100, 999);
+      address.city = Faker.Helpers.shuffle(definitions.ville_maine_et_loire).slice(0, 1)[0];
+      address.state = "Maine et Loire";
+      address.type = "shipping";
+      customer.addresses.push(address);
+      break;
+    }
   }
 
-  customer.contact = {}; 
+  customer.contacts = [];
   for (var j = 1; j <= 3; j++) {
-        switch ((j)) {
-        case 1:
-          countContactID ++;
-          customer.contact.admin = {};
-          customer.contact.admin.contactID = countContactID;
-          customer.contact.admin.firstName = Faker.random.first_name();
-          customer.contact.admin.lastName = Faker.random.last_name();
-          customer.contact.admin.email = Faker.Helpers.slugify(customer.contact.admin.firstName.toLowerCase()) + "." 
-                                + Faker.Helpers.slugify(customer.contact.admin.lastName.toLowerCase()) + "@" 
-                                + customerInternetDomain;
-          customer.contact.admin.phone = Faker.Helpers.replaceSymbolWithNumber("02 ## ## ## ##");
-          customer.contact.admin.avatar = Faker.Image.avatar();
-          break;
-          case 2:
-            countContactID ++;
-            customer.contact.sales = {};
-            customer.contact.sales.contactID = countContactID;
-            customer.contact.sales.firstName = Faker.random.first_name();
-            customer.contact.sales.lastName = Faker.random.last_name();
-            customer.contact.sales.email = Faker.Helpers.slugify(customer.contact.sales.firstName.toLowerCase()) + "." 
-                                  + Faker.Helpers.slugify(customer.contact.sales.lastName.toLowerCase()) + "@" 
-                                  + customerInternetDomain;
-            customer.contact.sales.phone = Faker.Helpers.replaceSymbolWithNumber("06 ## ## ## ##");
-            customer.contact.sales.avatar = Faker.Image.avatar();
-            break;
-          case 3:
-            countContactID ++;
-            customer.contact.technical = {};
-            customer.contact.technical.contactID = countContactID;
-            customer.contact.technical.firstName = Faker.random.first_name();
-            customer.contact.technical.lastName = Faker.random.last_name();
-            customer.contact.technical.email = Faker.Helpers.slugify(customer.contact.technical.firstName.toLowerCase()) + "." 
-                                  + Faker.Helpers.slugify(customer.contact.technical.lastName.toLowerCase()) + "@" 
-                                  + customerInternetDomain;
-            customer.contact.technical.phone = Faker.Helpers.replaceSymbolWithNumber("01 ## ## ## ##"); 
-            customer.contact.technical.avatar = Faker.Image.avatar();          
-            break;
-        }
-
+    switch ((j)) {
+      case 1:
+        countContactID ++;
+        contact = {};
+        contact.contactId = countContactID;
+        contact.firstName = Faker.random.first_name();
+        contact.lastName = Faker.random.last_name();
+        contact.email = Faker.Helpers.slugify(contact.firstName.toLowerCase()) + "." + Faker.Helpers.slugify(contact.lastName.toLowerCase()) + "@" + customerInternetDomain;
+        contact.phone = Faker.Helpers.replaceSymbolWithNumber("02 ## ## ## ##");
+        contact.avatar = Faker.Image.avatar();
+        contact.type = "admin";
+        customer.contacts.push(contact);
+        break;
+      case 2:
+        countContactID ++;
+        contact = {};
+        contact.contactId = countContactID;
+        contact.firstName = Faker.random.first_name();
+        contact.lastName = Faker.random.last_name();
+        contact.email = Faker.Helpers.slugify(contact.firstName.toLowerCase()) + "." + Faker.Helpers.slugify(contact.lastName.toLowerCase()) + "@" + customerInternetDomain;
+        contact.phone = Faker.Helpers.replaceSymbolWithNumber("06 ## ## ## ##");
+        contact.avatar = Faker.Image.avatar();
+        contact.type = "sales";
+        customer.contacts.push(contact);
+        break;
+      case 3:
+        countContactID ++;
+        contact = {};
+        contact.contactId = countContactID;
+        contact.firstName = Faker.random.first_name();
+        contact.lastName = Faker.random.last_name();
+        contact.email = Faker.Helpers.slugify(contact.firstName.toLowerCase()) + "." + Faker.Helpers.slugify(contact.lastName.toLowerCase()) + "@" + customerInternetDomain;
+        contact.phone = Faker.Helpers.replaceSymbolWithNumber("01 ## ## ## ##"); 
+        contact.avatar = Faker.Image.avatar();       
+        contact.type = "tech";
+        customer.contacts.push(contact);
+        break;
+    }
+  
   }
 
-  customer.createDate = Faker.Date.between(from, now);
-  customer.updateDate = Faker.Date.between(customer.createDate, now);
+  customer.orders = [];
+  for (var a = 1; a <= orderLength; a++) {
 
+    var order = {};
+    order.id = a;
+    order.orderDescription = Faker.Helpers.shuffle(definitions.order_type1).slice(0, 1) + " " + Faker.Address.streetName();
+
+    // var orderLineNumber = Faker.random.numberlowhigh(3,10);
+    // var orderLines = [];
+    // var amount = 0;
+    // var orderLineId = 0;
+    // for (var j = 1; j <= orderLineNumber ; j++) {
+    //   orderLineId++;
+    //   line = {};
+    //   line.lineId = orderLineId;
+    //   line.productId = Faker.random.numberlowhigh(1,10);
+    //   line.quantity = Faker.random.numberlowhigh(1,5) + "00";
+    //   line.unitPrice = Faker.random.numberlowhigh(1,3) + Faker.Helpers.shuffle(definitions.product_price_extention).slice(0, 1);
+    //   line.lineAmount = line.unitPrice * line.quantity;
+    //   amount = amount + line.lineAmount;
+    //   orderLines.push(line);
+    // }
+
+    //order.lines = orderLines;
+    
+    order.amount = Faker.random.numberlowhigh(1000,5000);
+    var createDate = Faker.Date.between(from, now);
+    var shipDate = Faker.Date.future(1000, createDate);
+    var updateDate = Faker.Date.between(createDate, shipDate);
+
+    order.createDate = createDate;
+    order.updateDate = updateDate;
+    order.shipDate = shipDate;
+  
+    customer.orders.push(order);
+
+  };
 
   // log the last to console
   if (i == customerLength) {
--- create-all.sh
+++ create-all.sh
@@ -6,19 +6,13 @@ touch fake-product-snrf.json
 touch fake-customer-qn.json
 touch fake-customer-fta.json
 touch fake-customer-snrf.json
-touch fake-order-qn.json
-touch fake-order-fta.json
-touch fake-order-snrf.json
 
 node beesearch/fake-product.js qn $1
 node beesearch/fake-product.js fta $1
 node beesearch/fake-product.js snrf $1
-node beesearch/fake-customer.js qn $1
-node beesearch/fake-customer.js fta $1
-node beesearch/fake-customer.js snrf $1
-node beesearch/fake-order.js qn $2
-node beesearch/fake-order.js fta $2
-node beesearch/fake-order.js snrf $2
+node beesearch/fake-customer.js qn $1 $2
+node beesearch/fake-customer.js fta $1 $2
+node beesearch/fake-customer.js snrf $1 $2
 
 wc -l fake-product-qn.json
 wc -l fake-product-fta.json
@@ -26,10 +20,6 @@ wc -l fake-product-snrf.json
 wc -l fake-customer-qn.json
 wc -l fake-customer-fta.json
 wc -l fake-customer-snrf.json
-wc -l fake-order-qn.json
-wc -l fake-order-fta.json
-wc -l fake-order-snrf.json
-
 
 echo "Send log to Elasticsearch ? [y/n]"
 
@@ -38,15 +28,15 @@ read touche
 case $touche in
 	y)
 		echo "Indexing.... please wait"
-		cat fake-product-qn.json | nc -q 500 localhost 3401
-		cat fake-customer-qn.json | nc -q 500 localhost 3402
-		cat fake-order-qn.json | nc -q 500 localhost 3403
-		cat fake-product-fta.json | nc -q 500 localhost 3404
-		cat fake-customer-fta.json | nc -q 500 localhost 3405
-		cat fake-order-fta.json | nc -q 500 localhost 3406
-		cat fake-product-snrf.json | nc -q 500 localhost 3407
-		cat fake-customer-snrf.json | nc -q 500 localhost 3408
-		cat fake-order-snrf.json | nc -q 500 localhost 3409
+		cat fake-product-qn.json | nc localhost 3401
+		cat fake-customer-qn.json | nc localhost 3402
+
+		cat fake-product-fta.json | nc localhost 3411
+		cat fake-customer-fta.json | nc localhost 3412
+
+		cat fake-product-snrf.json | nc localhost 3421
+		cat fake-customer-snrf.json | nc localhost 3422
+
 		;;
 	n)
 		echo "Bye"
