5681d7cdf828401fb68e2b62f3e7021363fcdead
robbee <alexandre.bonte@beesearch.fr>
2014-07-31 17:48:42 +0200

    create order flat for csv

---
+++ beesearch/fake-customer-flat.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+var Faker= require('../index');
+var definitions = require('../lib/definitions');
+
+// var zmq = require('zmq')
+//   , sock = zmq.socket('push');
+
+// sock.bindSync('tcp://127.0.0.1:3500');
+// console.log('### Producer bound to port 3500');
+
+// setInterval(function(){
+//   console.log('sending work');
+//   sock.send('some work');
+// }, 2000);
+
+
+// Customer setup
+var subsidiary = process.argv[2];
+var customerLength = process.argv[3];
+var orderLength = process.argv[4];
+
+// check subsidiary arg
+if (subsidiary == "snrf") {
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
+  var fileName = 'fake-customer-' + subsidiary + '.csv'
+  //var product_qualif = "product_qualif_" + subsidiary;
+} else if (subsidiary == "fta") {
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
+  var fileName = 'fake-customer-' + subsidiary + '.csv'
+} else if (subsidiary == "qn") {
+  console.log("### Starting  generating " + customerLength + " customers with " + orderLength + " orders for "+ subsidiary);
+  var fileName = 'fake-customer-' + subsidiary + '.csv'
+} else {
+  console.log("### Error subsidiary must be 'snrf', 'fta' or 'qn'. Please retry...");
+  process.exit(1);
+}
+
+var countContactID = 0;
+
+var from = new Date(2005, 1, 1, 8, 0, 0, 0);
+var now = new Date(2012, 27, 5, 18, 01, 02, 0);
+
+fs.unlinkSync(fileName);
+
+// generate customer file
+for (var i = 1; i <= customerLength; i++) {
+
+  var customer = {};
+  var customerName = Faker.Company.companyName();
+  var customerInternetDomain = Faker.Helpers.slugify(customerName.toLowerCase() + "." + Faker.random.domain_suffix());
+
+  customer = {};
+  customer.companyId = i;
+  customer.name = customerName;
+  customer.catchPhrase = Faker.Company.catchPhrase();
+  customer.siren = Faker.Helpers.replaceSymbolWithNumber("### ### ###");
+
+
+
+
+
+  // log the last to console
+  if (i == customerLength) {
+    console.log("--> Last customer");
+    console.log(JSON.stringify(customer, null, 2));
+  }
+
+  // generate file
+  var row = '';
+  //row += JSON.stringify(customer);
+  //row = json2csv.convert(customer, ["companyId", "name", "catchPhrase", "siren"], true);
+  row = customer.companyId + ";" + customer.name + ";" + customer.catchPhrase + ";" + customer.siren; 
+  row += '\n';
+  fs.appendFileSync(fileName, row);
+
+};
+
+console.log("--> File [" + fileName + "] generated successfully !");
+
+
---
+++ beesearch/fake-order-flat.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+var Faker= require('../index');
+var definitions = require('../lib/definitions');
+
+// order setup
+var subsidiary = process.argv[2];
+var orderLength = process.argv[3];
+
+// check subsidiary arg
+if (subsidiary == "snrf") {
+  console.log("### Starting  generating " + orderLength + " orders for " + subsidiary);
+  var fileName = 'fake-order-' + subsidiary + '.csv'
+  //var product_qualif = "product_qualif_" + subsidiary;
+} else if (subsidiary == "fta") {
+  console.log("### Starting  generating " + orderLength + " orders for " + subsidiary);
+  var fileName = 'fake-order-' + subsidiary + '.csv'
+} else if (subsidiary == "qn") {
+  console.log("### Starting  generating " + orderLength + " orders for " + subsidiary);
+  var fileName = 'fake-order-' + subsidiary + '.csv'
+} else {
+  console.log("### Error subsidiary must be 'snrf', 'fta' or 'qn'. Please retry...");
+  process.exit(1);
+}
+
+
+var from = new Date(2005, 1, 1, 8, 0, 0, 0);
+var now = new Date(2012, 27, 5, 18, 01, 02, 0);
+
+//var nowParse = Date.parse(now);
+//console.log("nowParse " + nowParse);
+
+fs.unlinkSync(fileName);
+
+// generate order file
+var orderLineId = 0;
+for (var i = 1; i <= orderLength; i++) {
+
+  var order = {};
+  
+  order.id = i;
+  order.customerId = Faker.random.numberlowhigh(1,1000)
+  order.orderDescription = Faker.Helpers.shuffle(definitions.order_type1).slice(0, 1) + " "
+                        + Faker.Address.streetName();
+
+  var orderLineNumber = Faker.random.numberlowhigh(3,10);
+  var orderLines = [];
+  var amount = 0;
+ 
+  order.amount = Faker.random.numberlowhigh(100,1000);
+  order.lines = orderLines;
+  var createDate = Faker.Date.between(from, now);
+  var shipDate = Faker.Date.future(1000, createDate);
+  var updateDate = Faker.Date.between(createDate, shipDate);
+
+  order.createDate = createDate;
+  order.updateDate = updateDate;
+  order.shipDate = shipDate;
+
+  // log the last to console
+  if (i == orderLength) {
+    console.log("--> Last order");
+    console.log(JSON.stringify(order, null, 2));
+  }
+
+  // generate file
+  var row = '';
+  row = order.id + ";" + order.customerId + ";" + order.orderDescription + ";" + order.amount + ";" + order.createDate + ";" + order.updateDate + ";" + order.shipDate; 
+  row += '\n';
+  fs.appendFileSync(fileName, row);
+
+};
+
+console.log("--> File [" + fileName + "] generated successfully !");
+
+
